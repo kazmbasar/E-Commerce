b24ceff320524b965cd8da953b9f64857a6e8473
kazmbasar <kazmbasar@gmail.com>
2024-02-28 14:18:00 +0300

    .

--- Business/Abstract/ICategoryService.cs
+++ Business/Abstract/ICategoryService.cs
@@ -1,16 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Core.Utilities.Results;
 using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Business.Abstract
 {
     public interface ICategoryService
     {
-        List<Category> GetAll();
-        Category GetById(int categoryId);
-
+        IDataResult<List<Category>> GetAll();
+        IDataResult<Category> GetById(int categoryId);
     }
 }
